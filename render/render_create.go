a1f8062912fe16cdd543f9b76d3a9db558870f76
Andrew Mayer <andrewmayer00@gmail.com>
2022-06-30 14:44:04 -0500

    add template id support

--- render/render_create.go
+++ render/render_create.go
@@ -15,26 +15,43 @@ func (c *RenderClient) Create(options *CreateRenderOptions) (*Render, error) {
 	if options == nil {
 		return nil, fmt.Errorf("must provide CreateRenderOptions to create a render")
 	}
-
-	if options.Html == "" && options.FileName == "" {
-		return nil, fmt.Errorf("must provide html or a file to create a render")
+	// No XOR in golang
+	
+	count := 0
+	
+	if options.Html != "" {
+		count++
 	}
-
-	if options.Html != "" && options.FileName != "" {
-		return nil, fmt.Errorf("either Html or FileName can be provided")
-	}
-
+	
 	if options.FileName != "" {
-
-		file, err := ioutil.ReadFile(options.FileName)
-
-		if err != nil {
-			return nil, err
-		}
-		options.Html = string(file)
+		count++
+	}
+	
+	if options.TemplateId != "" {
+		count++
+	}
+	
+	if count == 0 {
+		return nil, fmt.Errorf("Must provide Html, FileName, or TemplateId to create a render")
 	}
 
-	options.Html = base64.StdEncoding.EncodeToString([]byte(options.Html))
+	if count > 1 {
+		return nil, fmt.Errorf("Only one of the following can be provided at a time: Html, FileName, or TemplateId")
+	}
+	
+	if options.TemplateId == "" {
+		if options.FileName != "" {
+
+			file, err := ioutil.ReadFile(options.FileName)
+
+			if err != nil {
+				return nil, err
+			}
+			options.Html = string(file)
+		}
+
+		options.Html = base64.StdEncoding.EncodeToString([]byte(options.Html))
+	}
 
 	payload, err := json.Marshal(options)
 
